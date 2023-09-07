f68abbc7c68cffe2ea6847da4c3cf77b1b4b7f11
Uladzislau Dauhashei <v.dolgoshey@gmail.com>
2023-09-07 14:36:46 +0200

    added circle strategy + implemented strategy switch

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const KILOMETERS_IN_DEGREE = 111;
 
+const strategies = [{
+  name: "NORTH_AND_BACK",
+  getRouteCoordinates: getGoNorthAndBackRouteCoords,
+  isUsed: false
+}, {
+  name: "NORTH_AND_WEST_AND_SOUTH_AND_BACK",
+  getRouteCoordinates: getGoNorthThenWestThenSouthThenEastCoords,
+  isUsed: false
+}];
+
 const form = document.getElementById("coordinates");
 form.addEventListener("submit", calculateRoute);
 
@@ -55,7 +65,31 @@ function getAverageOfTwo(first, second) {
 
 function getRouteCoords() {
   const startCoords = { latitude: getStartLatitude(), longitude: getStartLongitude() };
-  return getGoNorthAndBackRouteCoords(startCoords);
+  markAllStrategiesUnusedIfNeeded(strategies);
+  let currentStrategyIndex = 0;
+  while (currentStrategyIndex < strategies.length) {
+    if (strategies[currentStrategyIndex].isUsed) {
+      currentStrategyIndex++;
+    } else {
+      const routeCoords = strategies[currentStrategyIndex].getRouteCoordinates(startCoords);
+      strategies[currentStrategyIndex].isUsed = true;
+      return routeCoords;
+    }
+  }
+}
+
+function markAllStrategiesUnusedIfNeeded(strategies) {
+  if (isAllStrategiesUsed(strategies)) {
+    markAllStrategiesUnused(strategies);
+  }
+}
+
+function isAllStrategiesUsed(strategies) {
+  return strategies.every(strategy => strategy.isUsed);
+}
+
+function markAllStrategiesUnused(strategies) {
+  strategies = strategies.map(strategy => strategy.isUsed = false);
 }
 
 function getMapUrl(routeCoords) {
@@ -92,25 +126,59 @@ function getStartLatitudeInput() {
 
 // route strategies
 
+// Strategy #1: Go North and back
 function getGoNorthAndBackRouteCoords(startCoords) {
   const startLongitude = startCoords.longitude;
   const startLatitude = startCoords.latitude;
   const northPointLongitude = startLongitude;
-  const northPointLatitude = getNorthPointLatitude(startLatitude);
+  const northPointLatitude = getForwardAndBackRouteNorthPointLatitude(startLatitude);
   const finishLongitude = startCoords.longitude;
   const finishLatitude = startCoords.latitude;
   const routeCoords = [{ latitude: startLatitude, longitude: startLongitude }, { latitude: northPointLatitude, longitude: northPointLongitude }, { latitude: finishLatitude, longitude: finishLongitude }];
   return routeCoords;
 }
 
-function getNorthPointLatitude(startLatitude) {
-  const latitudeDelta = getLatitudeDelta();
+function getForwardAndBackRouteNorthPointLatitude(startLatitude) {
+  const latitudeDelta = getForwardAndBackRouteLatitudeDelta();
   return (parseFloat(startLatitude) + latitudeDelta).toString();
 }
 
-function getLatitudeDelta() {
+function getForwardAndBackRouteLatitudeDelta() {
   const duration = document.getElementById("duration").value;
   const speed = document.getElementById("speed").value;
   const distance = ((duration / 60) * speed) / 2;
+  return distance / KILOMETERS_IN_DEGREE;
+}
+
+// Strategy #2: Go North, then West, then South, then back
+function getGoNorthThenWestThenSouthThenEastCoords(startCoords) {
+  const startLongitude = startCoords.longitude;
+  const startLatitude = startCoords.latitude;
+  const northPointLongitude = startLongitude;
+  const northPointLatitude = getCircleRouteNorthPointLatitude(startLatitude);
+  const westPointLongitude = getCircleRouteWestPointLongitude(startLongitude);
+  const westPointLatitude = northPointLatitude;
+  const eastPointLongitude = westPointLongitude;
+  const eastPointLatitude = startLatitude;
+  const finishLongitude = startCoords.longitude;
+  const finishLatitude = startCoords.latitude;
+  const routeCoords = [{ latitude: startLatitude, longitude: startLongitude }, { latitude: northPointLatitude, longitude: northPointLongitude }, { latitude: westPointLatitude, longitude: westPointLongitude }, { latitude: eastPointLatitude, longitude: eastPointLongitude }, { latitude: finishLatitude, longitude: finishLongitude }];
+  return routeCoords;
+}
+
+function getCircleRouteNorthPointLatitude(startLatitude) {
+  const latitudeDelta = getCircleRouteLatitudeOrLongitudeDelta();
+  return (parseFloat(startLatitude) + latitudeDelta).toString();
+}
+
+function getCircleRouteWestPointLongitude(startLongitude) {
+  const longitudeDelta = getCircleRouteLatitudeOrLongitudeDelta();
+  return (parseFloat(startLongitude) - longitudeDelta).toString();
+}
+
+function getCircleRouteLatitudeOrLongitudeDelta() {
+  const duration = document.getElementById("duration").value;
+  const speed = document.getElementById("speed").value;
+  const distance = ((duration / 60) * speed) / 4;
   return distance / KILOMETERS_IN_DEGREE;
 }
